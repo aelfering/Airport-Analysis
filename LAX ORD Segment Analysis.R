5be7c8f118341fe32d81b03cb5728577a8703495
Alex Elfering <alexelfering@Alexs-MacBook-Pro.local>
2020-05-16 14:08:01 -0500

    Removed unnecessary visualizations

--- LAX ORD Segment Analysis.R
+++ LAX ORD Segment Analysis.R
@@ -14,12 +14,12 @@ db1ba_huge <- list.files("/Users/alexelfering/Desktop/DB1B Whole", pattern = "*.
 db1badf_huge <- rbindlist(lapply(db1ba_huge, fread))
 
 # What percent of passengers fly between Chicago and Los Angeles Non-Stop?
-non_stop_PHX_LAX <- db1badf_huge %>%
+non_stop_CHI_LAX <- db1badf_huge %>%
   filter(!TICKET_CARRIER %in% c('--', '99'),
          ORIGIN %in% c('ORD', 'MDW'),
-         DEST %in% c('LAX'),
-         OP_CARRIER_CHANGE == 0) %>%
+         DEST %in% c('LAX')) %>%
   mutate(AIRPORT_GROUP = gsub('\\:', ' ', AIRPORT_GROUP)) %>%
+  # Removing the initial origin and final destination regarding connections
   mutate(TOTAL_CONNECTIONS = sapply(strsplit(AIRPORT_GROUP, " "), length)-2) %>%
   filter(TOTAL_CONNECTIONS == 0) %>%
   group_by(TICKET_CARRIER) %>%
@@ -28,41 +28,12 @@ non_stop_PHX_LAX <- db1badf_huge %>%
   mutate(QUARTERLY_PAX = sum(PAX)) %>%
   mutate(MKT_SHARE = PAX/QUARTERLY_PAX)
 
-ggplot(non_stop_PHX_LAX,
-       aes(x = reorder(TICKET_CARRIER, MKT_SHARE),
-           y = MKT_SHARE)) +
-  coord_flip() +
-  geom_bar(stat = 'identity',
-           position = 'identity',
-           color = 'black',
-           fill = '#5671d4') +
-  geom_text(aes(label = percent(MKT_SHARE)), 
-            position = position_dodge(width=1), 
-            hjust = 1.25) +
-  geom_hline(yintercept = 0) +
-  scale_y_continuous(labels = scales::percent) +
-  labs(title = 'United and American Airlines Dominated the Chicago-Los Angeles Segment',
-       subtitle = 'Q1 of 2019',
-       y = 'Quarterly Market Share',
-       x = '',
-       caption = 'Source: DB1B\nVisualization by Alex Elfering\nSouthwest flies out of Chicago Midway') +
-  theme(plot.title = element_text(face = 'bold', size = 18, family = 'Arial'),
-        plot.subtitle = element_text(size = 15, family = 'Arial'),
-        plot.caption = element_text(size = 12, family = 'Arial'),
-        axis.title = element_text(size = 12, family = 'Arial'),
-        axis.text = element_text(size = 12, family = 'Arial'),
-        strip.text = ggplot2::element_text(size = 12, hjust = 0, face = 'bold', color = 'brown', family = 'Arial'),
-        strip.background = element_rect(fill = NA),
-        panel.background = ggplot2::element_blank(),
-        axis.line = element_line(colour = "#222222", linetype = "solid"),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x = ggplot2::element_line(color = "#dedede", linetype = 'dashed')) 
-
 # What passengers fly to Los Angeles via Chicago?
 one_stop_PHX_LAX <- db1badf_huge %>%
   filter(!TICKET_CARRIER %in% c('--', '99')) %>%
   filter(DEST %in% c('LAX')) %>%
   mutate(AIRPORT_GROUP = gsub('\\:', ' ', AIRPORT_GROUP)) %>%
+  # Removing the initial origin and final destination regarding connections
   mutate(TOTAL_CONNECTIONS = sapply(strsplit(AIRPORT_GROUP, " "), length)-2) %>%
   filter(grepl('ORD LAX', AIRPORT_GROUP) | grepl('MDW LAX', AIRPORT_GROUP),
          TOTAL_CONNECTIONS > 0) %>%
@@ -72,36 +43,6 @@ one_stop_PHX_LAX <- db1badf_huge %>%
   mutate(QUARTERLY_PAX = sum(TOTAL_PAX)) %>%
   ungroup() %>%
   mutate(MKT_SHARE = TOTAL_PAX/QUARTERLY_PAX)
-
-ggplot(one_stop_PHX_LAX,
-       aes(x = reorder(TICKET_CARRIER, MKT_SHARE),
-           y = MKT_SHARE)) +
-  coord_flip() +
-  geom_bar(stat = 'identity',
-           position = 'identity',
-           color = 'black',
-           fill = '#5671d4') +
-  geom_text(aes(label = percent(MKT_SHARE)), 
-            position = position_dodge(width=1), 
-            hjust = 1.25) +
-  geom_hline(yintercept = 0) +
-  scale_y_continuous(labels = scales::percent) +
-  labs(title = 'United and Southwest Airlines Connect More Passengers Through Chicago to LAX',
-       subtitle = 'Q1 of 2019',
-       y = 'Quarterly Market Share',
-       x = '',
-       caption = 'Source: DB1B\nVisualization by Alex Elfering\nSouthwest flies out of Chicago Midway. Alaska does not connect passengers in Chicago.') +
-  theme(plot.title = element_text(face = 'bold', size = 18, family = 'Arial'),
-        plot.subtitle = element_text(size = 15, family = 'Arial'),
-        plot.caption = element_text(size = 12, family = 'Arial'),
-        axis.title = element_text(size = 12, family = 'Arial'),
-        axis.text = element_text(size = 12, family = 'Arial'),
-        strip.text = ggplot2::element_text(size = 12, hjust = 0, face = 'bold', color = 'brown', family = 'Arial'),
-        strip.background = element_rect(fill = NA),
-        panel.background = ggplot2::element_blank(),
-        axis.line = element_line(colour = "#222222", linetype = "solid"),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x = ggplot2::element_line(color = "#dedede", linetype = 'dashed')) 
 
 # What is the combined market share for connecting and non-stop?
 con_non <- merge(one_stop_PHX_LAX, non_stop_PHX_LAX, all.x = TRUE, all.y = TRUE, by = c('TICKET_CARRIER' = 'TICKET_CARRIER'))
@@ -113,7 +54,9 @@ con_non_mkt_share <- con_non %>%
   replace(is.na(.), 0) %>%
   mutate(Total_Pax = Multi_Stop + Non_Stop) %>%
   mutate(Quaterly_Pax = sum(Total_Pax)) %>%
-  mutate(MKT_SHARE = Total_Pax/Quaterly_Pax)
+  mutate(Mkt_Share = Total_Pax/Quaterly_Pax,
+         Pct_Connect = Multi_Stop/Total_Pax,
+         Pct_Nonstop = Non_Stop/Total_Pax)
 
 ggplot(con_non_mkt_share,
        aes(x = reorder(Airline, MKT_SHARE),
@@ -148,8 +91,7 @@ ggplot(con_non_mkt_share,
 # What origin feeds Chicago to Los Angeles by carrier?
 
 top_origin_carrier <- db1badf_huge %>%
-  filter(!TICKET_CARRIER %in% c('--', '99'),
-         OP_CARRIER_CHANGE == 0) %>%
+  filter(!TICKET_CARRIER %in% c('--', '99')) %>%
   filter(DEST %in% c('LAX')) %>%
   mutate(AIRPORT_GROUP = gsub('\\:', ' ', AIRPORT_GROUP)) %>%
   mutate(TOTAL_CONNECTIONS = sapply(strsplit(AIRPORT_GROUP, " "), length)-2) %>%
