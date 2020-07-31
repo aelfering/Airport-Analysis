052748ff9064363d2e2c7cb8ce6219e369625218
Alex Elfering <alexelfering@Alexs-MacBook-Pro.local>
2020-07-31 10:07:07 -0500

    Expanded functions

--- Visual Approach Aircraft Costs Tidying/AC Tidy Script.R
+++ Visual Approach Aircraft Costs Tidying/AC Tidy Script.R
@@ -1,22 +1,37 @@
-#install.packages("readxl")
-
 library(readxl)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(ggplot2)
 
-my_data <- read_excel("2020-07-27_Aircraft_Costs-2.xls")
-years <- c(2000:2020)
+setwd("~/Documents/GitHub/Airport-Analysis/Visual Approach Aircraft Costs Tidying")
 
-y  <- NULL
-for (i in years) {
-  year <- ifelse(grepl(i, colnames(my_data)[1]), i, NA)
-  y <- as.data.frame(rbind(y, year))
-}
+#### wrangling the data ####
+files <- list.files(path = "~/Documents/GitHub/Airport-Analysis/Visual Approach Aircraft Costs Tidying/Data", pattern = "*.xls", full.names = T)
+airline.list <- sapply(files, read_excel, simplify=FALSE)
 
-year <- as.integer(dplyr::filter(y, !is.na(V1)))
-rate <- as.character(my_data[2,1])
-airline <- as.character(my_data[2,2])
-
+#### functions ####
 tidy_xl_vs <- function(df) {
-mark1 <- my_data %>%
+  # This function:
+  #   - identifies the year of the excel sheet and creates a variable for a column below
+  #   - cleans the overall excel spreadsheet and removes excess NA rows and columns
+  
+  
+  # identifying years and creating a column for them
+  years <- c(2000:2020)
+  y  <- NULL
+  for (i in years) {
+    year <- ifelse(grepl(i, colnames(df)[1]), i, NA)
+    y <- as.data.frame(rbind(y, year))
+  }
+  
+  year <- as.integer(dplyr::filter(y, !is.na(V1)))
+  
+  # removing unnecessary columns and rows
+  rate <- as.character(df[2,1])
+  airline <- as.character(df[2,2])
+  
+mark1 <- df %>%
   filter(!is.na(`...2`)) %>%
   mutate(Airline = airline,
          Frequency = rate)
@@ -24,10 +39,26 @@ mark1 <- my_data %>%
 colnames(mark1)[1] <- "Metric"
 colnames(mark1)[2] <- "Total"
 mark2 <- mark1[-1, ]
+mark2$Period <- year
 
 return(mark2)}
+identify.equipment <- function(df){
+  # This function:
+  #   - identifies the airline's equipment (aircraft) being analyzed
+  #   - also flags 'all' if it is an overall cost
+  #   - also runs substr on the airline column to return the airline's IATA code
+  
+  # identify the airline's equipment
+  df$Equipment <- ifelse(grepl('All', df$Airline), 'All', NA)
+  
+  # identify the airline
+  df$Airline <- substr(df$Airline, 1, 2)
+  
+  return(df)
+}
 
+#### putting the functions to work and creating the final data frame ####
+apply.tidy.list <- lapply(airline.list, tidy_xl_vs)
+lapply(apply.tidy.list, identify.equipment)
 
-files <- list.files(path = "~/Desktop/TestAircraft", pattern = "*.xls", full.names = T)
-
-tbl <- sapply(files, read_excel, simplify=FALSE)
+airline.cost.df <- as.data.frame(do.call(rbind.data.frame, apply.tidy.list))
